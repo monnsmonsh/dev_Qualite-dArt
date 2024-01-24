2342b314fc6cfdb89d4c8700f439c90776e0ba8f
Martín Rodríguez <monse.developer@gmail.com>
2024-01-23 23:12:57 -0600

    recu ReyMts

---
+++ Contacto/src/JS/sendEmail.js
@@ -0,0 +1,24 @@
+//https://formspree.io/f/mleqgeke
+const form = document.getElementById('form')
+
+async function handleSendEmail(event) {
+    event.preventDefault()
+
+    const fd = new FormData(this)
+
+    const response = await fetch('https://formspree.io/f/mleqgeke', {
+        method: 'POST',
+        body: fd,
+        headers: { 
+            Accept: 'application/json'
+        }
+    })
+
+    if(response.ok) {
+        this.reset()
+         alert('Mensaje enviado')
+     } else {
+         alert('Error al enviar el mensaje')
+    }
+}
+form.addEventListener('submit', handleSendEmail)
